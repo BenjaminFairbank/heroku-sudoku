308d660850f2c3da0ba27259faa31eec73ec6cb8
BenjaminFairbank <BenFairbank26@gmail.com>
2020-10-10 09:12:44 +0800

    Removed old { useState } imports

--- app/javascript/react/components/pages/Home.js
+++ app/javascript/react/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { setSize, setDifficulty } from '../../modules/game'
 import { Link } from 'react-router-dom'
