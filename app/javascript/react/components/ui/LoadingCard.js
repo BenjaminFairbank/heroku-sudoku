d81888ce4c15656d0bcb3e72281707091327f8f7
BenjaminFairbank <BenFairbank26@gmail.com>
2020-10-31 17:53:07 +0800

    Load imagery changed to CircularProgress, selected square style changes

--- app/javascript/react/components/ui/LoadingCard.js
+++ app/javascript/react/components/ui/LoadingCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardMedia } from '@material-ui/core'
+import { Card, Box, CircularProgress } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   loading: {
     height: 380,
     width: 380,
+    textAlign: 'center',
+  },
+  div: {
+    marginTop: 100,
   },
 }))
 
@@ -14,12 +18,14 @@ const LoadingCard = props => {
   const classes = useStyles()
 
   return (
-    <Card>
-      <CardMedia
-        image="https://s3.amazonaws.com/horizon-production/images/redux/loading-icon.gif"
-        alt="loading-icon"
-        className={classes.loading}
-      ></CardMedia>
+    <Card className={classes.loading}>
+      <Box className={classes.div}>
+        <CircularProgress
+          color='secondary'
+          size={180}
+          thickness={2}
+        />
+    </Box>
     </Card>
   )
 }
